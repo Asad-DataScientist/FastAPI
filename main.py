a30869e213496eddf6f5c69d0289076f2d3cb279
Asad Ahmed <asad.softwareengineer@gmail.com>
2024-05-18 04:19:25 +0500

    Fast API (GET & POST) methods

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
+# #  Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+# # .\.venv\Scripts\activate # IF NAME OF THE VENV IS .VENV
+
+# # There are four HTTP methods
+# # GET, PUT, POST, DELETE
+
+
+# # HTTP REQUESTS METHOD
+
+# # GET: FOR ONLY DATA RETREIVAL
+
+# # POST: SUBMITS AN ENTITY TO THE SPECIFIED RESOURCE
+
+# # PUT: IS USED TO REPLACE ALL CURRENT REPRESENTATION OF TARGET RESOURCE
+
+# # DELETE: DELETES THE SPECIFED RESOURCE
+
+
+# # store a list of user to our database and expose the API
+# # so that we can delete, update and get user and so on
+
+from typing import Optional, List
+from models import User, Role, Gender  # Ensure these are defined correctly in the models file
 from fastapi import FastAPI
-from typing import List
-from models import User, Gender, Role
-from uuid import uuid4
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
+# # Sample database initialized as a list of User instances
+
 db: List[User] = [
-    
     User(
-         id = uuid4(), 
-         first_name = 'Jamila',
-         last_name = 'Ahmed',
-         gender = Gender.female,
-         roles = [Role.student]
+        id = UUID ('e1652b25-b727-4168-ab7e-5fd0814fae53'), # passing I.D in string to keep it fixed
+        first_name='Jamila',
+        last_name='Ahmed',
+        gender=Gender.female,
+        roles=[Role.student]
     ),
 
     User(
-         id = uuid4(), 
-         first_name = 'Alex',
-         last_name = 'Jones',
-         gender = Gender.male,
-         roles = [Role.admin, Role.user]
-    )   
-
+        id = uuid4(),
+        first_name='Alex',
+        last_name='jones',
+        gender=Gender.male,
+        roles=[Role.user]
+    )
 ]
 
-# making get request for retreiving the data
-# There are four methods, GET, POST,  PUT, DELETE
-
-# endpoint
 @app.get('/')
-async def root ():
-    return {'Hello' : 'Asad'}
+def root():
+    return {'Hello': 'Hubex'}
 
-# endpoint
 @app.get('/api/v1/users')
 async def fetch_users():
     return db;
+
+@app.post('/api/v1/users')
+async def register_user(user: User):
+    db.append(user)
+    return {'id':user.id}
