2f7c8a2a2ad35b0406a2aaef59a3c8f96c245b60
Asad Ahmed <asad.softwareengineer@gmail.com>
2024-05-19 18:51:18 +0500

    Main File
    Contains the GET, POST, DELETE and PUT methods.

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-# #  Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-# # .\.venv\Scripts\activate # IF NAME OF THE VENV IS .VENV
 
-# # There are four HTTP methods
-# # GET, PUT, POST, DELETE
-
-
-# # HTTP REQUESTS METHOD
-
-# # GET: FOR ONLY DATA RETREIVAL
-
-# # POST: SUBMITS AN ENTITY TO THE SPECIFIED RESOURCE
-
-# # PUT: IS USED TO REPLACE ALL CURRENT REPRESENTATION OF TARGET RESOURCE
-
-# # DELETE: DELETES THE SPECIFED RESOURCE
+# HTTP REQUESTS METHOD
+# GET: FOR ONLY DATA RETREIVAL
+# POST: SUBMITS AN ENTITY TO THE SPECIFIED RESOURCE
+# PUT: IS USED TO REPLACE ALL CURRENT REPRESENTATION OF TARGET RESOURCE
+# DELETE: DELETES THE SPECIFED RESOURCE
 
 
-# # store a list of user to our database and expose the API
-# # so that we can delete, update and get user and so on
+# store a list of user to our database and expose the API
+# so that we can delete, update and get user and so on
 
 from typing import Optional, List
-from models import User, Role, Gender  # Ensure these are defined correctly in the models file
-from fastapi import FastAPI
+from models import User, Role, Gender, UserUpdateRequest  # Ensure these are defined correctly in the models file
+from fastapi import FastAPI, HTTPException
 from uuid import UUID, uuid4
 from pydantic import BaseModel
 from enum import Enum
 
 app = FastAPI()
 
-# # Sample database initialized as a list of User instances
+# Sample database initialized as a list of User instances
 
 db: List[User] = [
     User(
@@ -41,22 +31,76 @@ db: List[User] = [
 
     User(
         id = uuid4(),
-        first_name='Alex',
+        first_name='Alex',  
         last_name='jones',
         gender=Gender.male,
         roles=[Role.user]
+    ),
+
+    User(
+    id = UUID ('d150e319-8030-4910-ac6e-678b5f980b78'),
+    first_name = "Rahul",
+    last_name = "Puri",
+    gender = Gender.male,
+    roles = [
+      Role.admin
+    ]
+    ),
+
+    User(
+    id = uuid4(),
+    first_name = "Wahaj",
+    last_name = "Rasheed",
+    gender = Gender.male,
+    roles = [
+      Role.admin
+    ]
     )
 ]
 
+# BASIC GET
 @app.get('/')
 def root():
     return {'Hello': 'Hubex'}
 
+# GET METHOD
 @app.get('/api/v1/users')
 async def fetch_users():
     return db;
 
+# POST METHOD
 @app.post('/api/v1/users')
 async def register_user(user: User):
     db.append(user)
     return {'id':user.id}
+
+# DELETE METHOD
+@app.delete("/api/v1/users/{user_id}")
+async def delete(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
+    )
+
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            # if user_update.middle_name is not None:
+            #     user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+    raise HTTPException(
+            status_code = 404,
+            detail=f"user with id: {user_id} does not exists" 
+        )
